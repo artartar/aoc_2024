3dbab3b41e25989149aa0ef8a1cbfa158ff6594f
artartar <artsim3088@gmail.com>
2024-12-09 08:00:04 -0500

    WIP: Not useful, but I'm frustrated and need to save

--- Day2/day2_1.go
+++ Day2/day2_1.go
@@ -20,7 +20,9 @@ func main() {
 	scanner := bufio.NewScanner(data)
 	var numbers []int
 	increase_count := 0
-	decrease_count := 0
+	// decrease_count := 0
+	var failed_results [][]int
+
 	for scanner.Scan() {
 		line := scanner.Text()
 		split_line := strings.Split(line, " ")
@@ -30,48 +32,81 @@ func main() {
 			convert_me, _ := strconv.ParseInt(split_line[i], 10, 64)
 			numbers = append(numbers, int(convert_me))
 		}
-		if increase_validation(numbers) == true {
+		i_result, i_fail_return := increase_validation(numbers)
+		if i_result == true {
 			increase_count++
+		} else {
+			failed_results = append(failed_results, i_fail_return)
 		}
 
-		if decrease_validation(numbers) == true {
-			decrease_count++
+	// 	d_result, d_fail_return := decrease_validation(numbers)
+	// 	if d_result == true {
+	// 		decrease_count++
+	// 	} else {
+	// 		failed_results = append(failed_results, d_fail_return)
+	// 	}
+	}
+
+	for j := range len(failed_results) {
+		j_result, _ := increase_validation(failed_results[j])
+		if j_result == true {
+			increase_count++
 		}
 	}
-	final_count := increase_count + decrease_count
-	fmt.Printf("Number of safe reports: %d", final_count)
+	final_count := increase_count
+	fmt.Printf("Number of safe reports: %d\n", final_count)
+
 }
 
 // Check if the numbers in the array pass the Day 2 Part 1 test.
 // Must increase or decrease all the way through, no change is considered a failure
 // Change can be no more than 3
 // false == unsafe | true == safe
-func increase_validation(numbers []int) bool {
+func increase_validation(numbers []int) (bool, []int) {
 	iterator := 1
+	success_return := []int{}
 
 	for i := range len(numbers) - 1 {
 		test := numbers[iterator] - numbers[i]
 		if test > 0 && test <= 3 {
 			iterator++
 		} else {
-			iterator = 1
-			return false
+			var remove_error []int
+			if numbers[iterator] > numbers[i] {
+				remove_error = remove_value(numbers, iterator)
+			} else {
+				remove_error = remove_value(numbers, i)
+			}
+			return false, remove_error
 		}
 	}
-	return true
+	return true, success_return
 }
 
-func decrease_validation(numbers []int) bool {
+func decrease_validation(numbers []int) (bool, []int) {
 	iterator := 1
+	success_return := []int{}
 
 	for i := range len(numbers) - 1 {
 		test := numbers[i] - numbers[iterator]
 		if test > 0 && test <= 3 {
 			iterator++
 		} else {
-			iterator = 1
-			return false
+			var remove_error []int
+			fmt.Printf("Original: %v\niterator: %d\n", numbers, iterator)
+			if numbers[iterator] > numbers[i] {
+				remove_error = remove_value(numbers, iterator)
+			} else {
+				remove_error = remove_value(numbers, i)
+			}
+			
+			fmt.Printf("Mod: %v\n", remove_error)
+			return false, remove_error
 		}
 	}
-	return true
+	return true, success_return
+}
+
+func remove_value(numbers []int, s int) []int {
+	return append(numbers[:s], numbers[s+1:]...)
 }
